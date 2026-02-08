cc41d50bfa9344ae3a15e6fd6694c8326f7501ac
manhnguyenduc153 <manhvipyb1@gmail.com>
2026-02-08 10:14:30 +0700

    AI API solution

---
+++ NguyenDucManh_SE1884_A01_AIAPI/NguyenDucManh_SE1884_A01_FE/Extensions/ServicesRegister.cs
@@ -0,0 +1,13 @@
+﻿using Assignmen_PRN232__.Models;
+using Frontend.Handlers;
+
+namespace UsersApp.Extensions
+{
+    public static class ServicesRegister
+    {
+        public static void RegisterCustomServices(this IServiceCollection services)
+        {
+            
+        }
+    }
+}
